471ecde759911707f40b1d2391a7cd8a0da16377
atle <alextle24@gmail.com>
2024-01-15 10:42:42 -0700

    refractored middle to have a separate test documentation

--- middle.js
+++ middle.js
@@ -1,23 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2) === true) {
-    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
-
 const middle = function(arr) {
   let midArr = [];
   let midIndex = Math.ceil(arr.length / 2) - 1;
@@ -33,8 +13,5 @@ const middle = function(arr) {
   }
 };
 
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2,]), []);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7]), [4]);
+module.exports = middle;
+
