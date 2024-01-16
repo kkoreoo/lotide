f31ba8c7dc1269b37c379406305e40bf8fb4363a
atle <alextle24@gmail.com>
2024-01-15 20:24:16 -0700

    removed unnecessary lines of code

--- eqArrays.js
+++ eqArrays.js
@@ -1,21 +1,17 @@
 const eqArrays = function(arr1, arr2) {
+  let result = true; 
   if (arr1.length != arr2.length) {
-    console.log('hello');
     return false;
   }
   for (let i = 0; i < arr1.length; i++) {
-    if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
-      if (arr1[i].length !== arr2[i].length) {
-        return false;
-      }
-      
-      eqArrays(arr1[i], arr2[i]);
+    if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {   
+      result = eqArrays(arr1[i], arr2[i]);
 
     } else if (arr1[i] !== arr2[i]) {
       return false;
     }
   }
-  return true;
+  return result;
 };
 
 module.exports = eqArrays;
