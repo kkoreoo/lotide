212d4826a8cb039e2520ac2389081ce2798fddf7
atle <alextle24@gmail.com>
2024-01-15 20:04:42 -0700

    created flatten.js

--- flatten.js
+++ flatten.js
@@ -19,12 +19,7 @@ const assertArraysEqual = function(arr1, arr2) {
 };
 
 const flatten = function (arr) {
-  for (let i = 0; i < arr.length; i++) {
-    if (Array.isArray(arr[i])){
-      arr[i].pop();
-      }
-      console.log(arr);
-  }
+  return arr.flat();
 };
 
-flatten([1, 2, [3, 4], 5, [6]]) // => [1, 2, 3, 4, 5, 6]
+console.log(flatten([1, 2, [3, 4], 5, [6]])) // => [1, 2, 3, 4, 5, 6]
